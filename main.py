1695dd3028e723a10bccd5b162f7753364679a42
Natalie Wagner <nrwagner723@gmail.com>
2022-05-06 01:26:38 -0500

    created while loops to randomly generate and select the destination and restaurant

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import random
+
+# lists containing all the options the user can choose from
 destinations = ['Istanbul, Turkey', 'Machu Picchu, Peru', 'South Island, New Zealand', 'London, England', 'Tokyo, Japan']
 
 restaurants_istanbul = ['Olive', 'Gulhane Sark Sofrasi', 'Bitlisli', 'Garden Mezze', 'Neolokal']
@@ -13,3 +16,31 @@ entertainment_machu_picchu = ['see the Temple of the Sun', 'climb Huayna Picchu'
 entertainment_south_island = ['visit Queenstown, New Zealand', 'explore Milford Sound', 'walk the Kepler Track', 'explore Mount Cook National Park', 'hike Roy\'s Peak']
 entertainment_london = ['go on the Harry Potter walking tour', 'tresure hunt in HiddenCity', 'visit the Tower of London', 'go on a Ghost Bus Tour', 'visit Borough Market']
 entertainment_tokyo = ['rent a go-cart and drive around Tokyo', 'shop at Kappabashi', 'visit the Senso-Ji Buddhist Temple', 'go to The National Art Center', 'see sumo wrestling at Ryogoku Kokugikan Sumo Stadium']
+
+# while loop to pick destination
+user_input = ''
+
+while user_input != 'y':
+    user_destination = destinations[random.randint(0, 4)]
+    user_input = input(f'We have selected {user_destination} for your destination. Does this sound good? Enter y/n: ')
+
+# while loop to pick restaurant
+user_input1 = ''
+
+while user_input1 != 'y':
+    if user_destination == 'Istanbul, Turkey':
+        user_restaurant_istanbul = restaurants_istanbul[random.randint(0, 4)]
+        user_input1 = input(f'We have selected {user_restaurant_istanbul} for your restaurant. Does this sound good? Enter y/n: ')
+        print(user_restaurant_istanbul)
+    elif user_destination == 'Machu Picchu, Peru':
+        user_restaurant_machu_picchu = restaurants_machu_picchu[random.randint(0, 4)]
+        user_input1 = input(f'We have selected {user_restaurant_machu_picchu} for your restaurant. Does this sound good? Enter y/n: ')
+    elif user_destination == 'South Island, New Zealand':
+        user_restaurant_south_island = restaurants_south_island[random.randint(0, 4)]
+        user_input1 = input(f'We have selected {user_restaurant_south_island} for your restaurant. Does this sound good? Enter y/n: ')
+    elif user_destination == 'London, England':
+        user_restaurant_london = restaurants_london[random.randint(0, 4)]
+        user_input1 = input(f'We have selected {user_restaurant_london} for your restaurant. Does this sound good? Enter y/n: ')
+    elif user_destination == 'Tokyo, Japan':
+        user_restaurant_tokyo = restaurants_tokyo[random.randint(0, 4)]
+        user_input1 = input(f'We have selected {user_restaurant_tokyo} for your restaurant. Does this sound good? Enter y/n: ')
