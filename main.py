24db9d248da872ed376e6b0602dc4334cb1892a5
Natalie Wagner <nrwagner723@gmail.com>
2022-05-06 00:57:55 -0500

    added all of the lists that contain the destinations, restaurants, transportation methods, and entertainment options to main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+destinations = ['Istanbul, Turkey', 'Machu Picchu, Peru', 'South Island, New Zealand', 'London, England', 'Tokyo, Japan']
+
+restaurants_istanbul = ['Olive', 'Gulhane Sark Sofrasi', 'Bitlisli', 'Garden Mezze', 'Neolokal']
+restaurants_machu_picchu = ['Chullpi', 'Tree House', 'Cala Tratoria', 'Apu Inti', 'Toto\'s House']
+restaurants_south_island = ['Ortolana', 'The Sugar Club', 'The French Cafe', 'SidArt', 'Poderi Crisci']
+restaurants_london = ['Kitchen Table', 'Bob Bob Richard Soho', 'Kiln', 'Padella', 'The Clove Club']
+restaurants_tokyo = ['Narisawa', 'Ise Sueyoshi', 'Sushi Saito', 'Den', 'Hinokizaka']
+
+transportation = ['train', 'bus', 'ship', 'plane', 'tram']
+
+entertainment_istanbul = ['visit the Hagia Sophia', 'check out the Suleymaniye Mosque', 'tour the Topkapi Palace', 'visit the Dolmabahce Palace', 'admire the Beylerbeyi Palace']
+entertainment_machu_picchu = ['see the Temple of the Sun', 'climb Huayna Picchu', 'climb up to the Sun Gate', 'visit the Temple of the Moon and the Great Cavern', 'find the Intihuatana']
+entertainment_south_island = ['visit Queenstown, New Zealand', 'explore Milford Sound', 'walk the Kepler Track', 'explore Mount Cook National Park', 'hike Roy\'s Peak']
+entertainment_london = ['go on the Harry Potter walking tour', 'tresure hunt in HiddenCity', 'visit the Tower of London', 'go on a Ghost Bus Tour', 'visit Borough Market']
+entertainment_tokyo = ['rent a go-cart and drive around Tokyo', 'shop at Kappabashi', 'visit the Senso-Ji Buddhist Temple', 'go to The National Art Center', 'see sumo wrestling at Ryogoku Kokugikan Sumo Stadium']
